87b36ed61c1c0c2a8c3cbb38fc3bb7cbc3109c65
AYODYULA GANGA DURGA PRASAD <gangadurgaprasadayodyula@gmail.com>
2026-07-16 21:11:10 +0530

    basic variable declaration program

---
+++ src/variable_decl.java
@@ -0,0 +1,10 @@
+public class variable_decl {
+    public static void main(String[] args)
+    {
+        int age = 20;
+
+        String name = "Ganga";
+        System.out.println(age);
+        System.out.println(name);
+    }
+}
