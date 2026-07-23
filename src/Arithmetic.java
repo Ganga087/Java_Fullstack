00070d96c75248b1037fe89218bde1c06c74b9da
AYODYULA GANGA DURGA PRASAD <gangadurgaprasadayodyula@gmail.com>
2026-07-23 11:18:58 +0530

    Arithmetic operators

---
+++ src/Arithmetic.java
@@ -0,0 +1,17 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+
+
+        int a = 10;
+        int b= 20;
+        System.out.println("Addition of two numbers is :" + (a+b));
+        System.out.println("Subtraction of two numbers is :" + (a-b));
+        System.out.println("Multiplication of two numbers is :" + (a * b));
+        System.out.println("Division of two numbers is :" + (a / b));
+        System.out.println("Modulus of  two numbers is :" + (a % b));
+
+
+    }
+}
+
+
