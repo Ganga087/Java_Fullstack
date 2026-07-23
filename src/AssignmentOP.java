824939c67ce827f380074d44d3591ef969b8c2fa
AYODYULA GANGA DURGA PRASAD <gangadurgaprasadayodyula@gmail.com>
2026-07-23 11:27:59 +0530

    Assignment operators

---
+++ src/AssignmentOP.java
@@ -0,0 +1,15 @@
+public class AssignmentOP {
+    public static void main(String[] args)
+    {
+        int a=12;
+        int b=a++;
+        System.out.println(a);
+        System.out.println(b);
+        int c= a++ + ++a + a;
+        System.out.println(c);
+
+
+
+
+    }
+}
